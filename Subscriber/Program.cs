43ded106c8defcff3511c1a1ebc9b7e2d74620a6
tgondar <telmo.gondar@ventureslab.io>
2024-03-07 13:55:32 +0000

    there it is, them missing subscriber project

---
+++ Subscriber/Program.cs
@@ -0,0 +1,11 @@
+﻿using Subscriber;
+
+Console.WriteLine("Hello, World!");
+
+var rabbitReader = new RabbitReader();
+
+Console.WriteLine("Rabbit reader: ");
+
+rabbitReader.Read();
+
+Console.ReadLine();
---
+++ Subscriber/RabbitReader.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Subscriber
+{
+    public class RabbitReader
+    {
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitReader()
+        {
+            CreateConnection();
+        }
+
+        public void CreateConnection()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "user",
+                Password = "password",
+
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void Read()
+        {
+            _channel.QueueDeclare(queue: "productQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+
+            _channel.BasicConsume(queue: "productQueue", autoAck: true, consumer: consumer);
+        }
+    }
+}
